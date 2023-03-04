ab3e1e6ff73f17ea93e02cf665d5fd70846eafb4
Saúl Sosa Díaz <alu0101404141@ull.edu.es>
2023-03-04 13:53:35 +0000

    Terminado el quicksort, hay un bug en mergesort

--- Practica4/prueba.py
+++ Practica4/prueba.py
@@ -52,13 +52,13 @@ class MergeSort(DaC):
     result = []
     divide1 = []
     divide2 = []
-    for i in range(0,len(problem)//2):
+    for i in range(0, n//2):
       divide1.append(problem[i])
     result.append(divide1)
-    for i in range(len(problem)//2, len(problem)):
+    for i in range(n//2, n):
       divide2.append(problem[i])
     result.append(divide2)
-  
+
     return result
     
   def SolveSmall(self, problem: list):
@@ -94,8 +94,55 @@ class MergeSort(DaC):
         j += 1
     return result
 
+
+class QuickSort(DaC):
+
+  def Small(self, problem: list):
+    return True if len(problem) == 1 else False
+
+  def Divide(self, problem: list, n: int):
+    result = []
+    divide1 = []
+    divide2 = []
+    pivot = problem[len(problem) - 1]
+    i = 0
+    r = len(problem) - 1
+    while True:
+      while problem[i] < pivot:
+        i += 1
+      while problem[r] > pivot:
+        r -= 1
+      if (i >= r):
+        break
+      # swap
+      aux = problem[i]
+      problem[i] = problem[r]
+      problem[r] = aux
+
+    # swap
+    aux = problem[i]
+    problem[i] = problem[r]
+    problem[r] = aux
+
+    divide1 = problem[:r]
+    divide2 = problem[r:]
+    result.append(divide1)
+    result.append(divide2)
+
+    return result
+
+  def SolveSmall(self, problem: list):
+    return problem
+
+  def Combine(self, s1: list, s2: list):
+    result =  s1 + s2
     
-   
-a = MergeSort()
-v = [847, 166, 525, 342, 839, 965, 159, 961, 414, 337]
-print(a.Solve(v,len(v)))
+    return result
+
+def main():   
+  a = MergeSort()
+  v = [847, 166, 525, 342, 839, 965, 159, 961, 414, 337]
+  print(a.Solve(v,len(v)))
+
+if __name__ == "__main__":
+  main()
